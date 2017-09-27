25ddb744f9c69d6d1d7f900ac9221c07285c8d6a
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2017-09-27 19:00:54 +0530

    chore: Add sample data

--- app/steps/SampleDataContainer.js
+++ app/steps/SampleDataContainer.js
@@ -5,23 +5,10 @@ export default class SampleDataContainer extends Component {
 	renderData() {
 		return data.map((row, index) => (
 			<tr key={index}>
-				<td>{row.accommodates}</td>
-				<td>{row.bathrooms}</td>
-				<td>{row.bed_type}</td>
-				<td>{row.bedrooms}</td>
-				<td>{row.beds}</td>
-				<td>{row.date_from}</td>
-				<td>{row.date_to}</td>
-				<td>{row.has_availability}</td>
-				<td>{row.host_image}</td>
-				<td>{row.host_name}</td>
-				<td>{row.image}</td>
-				<td>{row.listing_url}</td>
-				<td>{"["}{row.location.lat}, {row.location.lon}{"]"}</td>
-				<td>{row.name}</td>
-				<td>{row.price}</td>
-				<td>{row.property_type}</td>
-				<td>{row.room_type}</td>
+				<td>{row.id}</td>
+				<td>{row.title}</td>
+				<td>{row.completed}</td>
+				<td>{row.createdAt}</td>
 			</tr>
 		))
 	}
@@ -29,7 +16,7 @@ export default class SampleDataContainer extends Component {
 	render() {
 		return (
 			<section className="single-step">
-				<h2>Indexing Airbnb Dataset</h2>
+				<h2>Preparing Sample Dataset</h2>
 				<p>
 					Every good app starts with data. We have prepared a sample Airbnb dataset to be indexed in our app. Hit the <strong>Next</strong> button at the bottom of the screen.
 				</p>
@@ -37,23 +24,10 @@ export default class SampleDataContainer extends Component {
 					<table className="highlight responsive-table">
 						<thead>
 							<tr>
-								<th>accommodates</th>
-								<th>bathrooms</th>
-								<th>bed_type</th>
-								<th>bedrooms</th>
-								<th>beds</th>
-								<th>date_from</th>
-								<th>date_to</th>
-								<th>has_availability</th>
-								<th>host_image</th>
-								<th>host_name</th>
-								<th>image</th>
-								<th>listing_url</th>
-								<th>location</th>
-								<th>name</th>
-								<th>price</th>
-								<th>property_type</th>
-								<th>room_type</th>
+								<th>id</th>
+								<th>title</th>
+								<th>completed</th>
+								<th>createdAt</th>
 							</tr>
 						</thead>
 						<tbody>

690d1cf2bb9c4100fe2b0132792f5b52c4520a66
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2017-10-03 01:08:22 +0530

    Add appbase-js methods

--- app/steps/DemoComponents.js
+++ app/steps/DemoComponents.js
@@ -43,66 +43,171 @@ const ReactiveListMarkup = `<pre style='color:#000000;background:#ffffff;'><span
 <span style='color:#808030; '>/</span><span style='color:#808030; '>></span>
 </pre>`;
 
+const IndexMarkup = `<pre style='color:#000000;background:#ffffff;'><span style='color:#800000; font-weight:bold; '>this</span><span style='color:#808030; '>.</span>appbaseRef<span style='color:#808030; '>.</span><span style='color:#797997; '>index</span><span style='color:#808030; '>(</span><span style='color:#800080; '>{</span>
+  type<span style='color:#800080; '>:</span> ES_TYPE<span style='color:#808030; '>,</span>
+  id<span style='color:#800080; '>:</span> id<span style='color:#808030; '>,</span>
+  body<span style='color:#800080; '>:</span> <span style='color:#800080; '>{</span><span style='color:#808030; '>.</span><span style='color:#808030; '>.</span><span style='color:#808030; '>.</span><span style='color:#800080; '>}</span>
+<span style='color:#800080; '>}</span><span style='color:#808030; '>)</span><span style='color:#808030; '>.</span>on<span style='color:#808030; '>(</span><span style='color:#800000; '>'</span><span style='color:#0000e6; '>data</span><span style='color:#800000; '>'</span><span style='color:#808030; '>,</span> <span style='color:#800000; font-weight:bold; '>function</span><span style='color:#808030; '>(</span>res<span style='color:#808030; '>)</span> <span style='color:#800080; '>{</span>
+  console<span style='color:#808030; '>.</span><span style='color:#800000; font-weight:bold; '>log</span><span style='color:#808030; '>(</span>res<span style='color:#808030; '>)</span>
+<span style='color:#800080; '>}</span><span style='color:#808030; '>)</span><span style='color:#800080; '>;</span>
+</pre>`;
+
+const DeleteMarkup = `<pre style='color:#000000;background:#ffffff;'><span style='color:#800000; font-weight:bold; '>this</span><span style='color:#808030; '>.</span>appbaseRef<span style='color:#808030; '>.</span><span style='color:#800000; font-weight:bold; '>delete</span><span style='color:#808030; '>(</span><span style='color:#800080; '>{</span>
+  type<span style='color:#800080; '>:</span> ES_TYPE<span style='color:#808030; '>,</span>
+  id<span style='color:#800080; '>:</span> todo<span style='color:#808030; '>.</span>id
+<span style='color:#800080; '>}</span><span style='color:#808030; '>)</span><span style='color:#808030; '>.</span>on<span style='color:#808030; '>(</span><span style='color:#800000; '>'</span><span style='color:#0000e6; '>data</span><span style='color:#800000; '>'</span><span style='color:#808030; '>,</span> <span style='color:#800000; font-weight:bold; '>function</span><span style='color:#808030; '>(</span>res<span style='color:#808030; '>)</span> <span style='color:#800080; '>{</span>
+  console<span style='color:#808030; '>.</span><span style='color:#800000; font-weight:bold; '>log</span><span style='color:#808030; '>(</span>res<span style='color:#808030; '>)</span>
+<span style='color:#800080; '>}</span><span style='color:#808030; '>)</span><span style='color:#800080; '>;</span>
+</pre>`;
+
+const SearchMarkup = `<pre style='color:#000000;background:#ffffff;'><span style='color:#800000; font-weight:bold; '>this</span><span style='color:#808030; '>.</span>appbaseRef<span style='color:#808030; '>.</span>search<span style='color:#808030; '>(</span><span style='color:#800080; '>{</span>
+  type<span style='color:#800080; '>:</span> ES_TYPE<span style='color:#808030; '>,</span>
+  body<span style='color:#800080; '>:</span> <span style='color:#800080; '>{</span>
+    query<span style='color:#800080; '>:</span> <span style='color:#800080; '>{</span>
+      match_all<span style='color:#800080; '>:</span> <span style='color:#800080; '>{</span><span style='color:#800080; '>}</span>
+    <span style='color:#800080; '>}</span>
+  <span style='color:#800080; '>}</span>
+<span style='color:#800080; '>}</span><span style='color:#808030; '>)</span><span style='color:#808030; '>.</span>on<span style='color:#808030; '>(</span><span style='color:#800000; '>'</span><span style='color:#0000e6; '>data</span><span style='color:#800000; '>'</span><span style='color:#808030; '>,</span> <span style='color:#800000; font-weight:bold; '>function</span><span style='color:#808030; '>(</span>res<span style='color:#808030; '>)</span> <span style='color:#800080; '>{</span>
+  console<span style='color:#808030; '>.</span><span style='color:#800000; font-weight:bold; '>log</span><span style='color:#808030; '>(</span>res<span style='color:#808030; '>)</span>
+<span style='color:#800080; '>}</span><span style='color:#808030; '>)</span><span style='color:#800080; '>;</span>
+</pre>`;
+
+const SearchStreamMarkup = `<pre style='color:#000000;background:#ffffff;'><span style='color:#800000; font-weight:bold; '>this</span><span style='color:#808030; '>.</span>appbaseRef<span style='color:#808030; '>.</span>searchStream<span style='color:#808030; '>(</span><span style='color:#800080; '>{</span>
+  type<span style='color:#800080; '>:</span> ES_TYPE<span style='color:#808030; '>,</span>
+  body<span style='color:#800080; '>:</span> <span style='color:#800080; '>{</span>
+    query<span style='color:#800080; '>:</span> <span style='color:#800080; '>{</span>
+      match_all<span style='color:#800080; '>:</span> <span style='color:#800080; '>{</span><span style='color:#800080; '>}</span>
+    <span style='color:#800080; '>}</span>
+  <span style='color:#800080; '>}</span>
+<span style='color:#800080; '>}</span><span style='color:#808030; '>)</span><span style='color:#808030; '>.</span>on<span style='color:#808030; '>(</span><span style='color:#800000; '>'</span><span style='color:#0000e6; '>data</span><span style='color:#800000; '>'</span><span style='color:#808030; '>,</span> <span style='color:#800000; font-weight:bold; '>function</span><span style='color:#808030; '>(</span>res<span style='color:#808030; '>)</span> <span style='color:#800080; '>{</span>
+  console<span style='color:#808030; '>.</span><span style='color:#800000; font-weight:bold; '>log</span><span style='color:#808030; '>(</span>res<span style='color:#808030; '>)</span>
+<span style='color:#800080; '>}</span><span style='color:#808030; '>)</span><span style='color:#800080; '>;</span>
+</pre>`;
+
 export default class DemoComponents extends Component {
-	render() {
-		return (
-			<section className="single-step">
-				<h2>Building the SimpleBeds UI</h2>
+  render() {
+    return (
+      <section className="single-step">
+        <h2>Building the TodoMVC App</h2>
 
-				<div className="docs-wrapper">
-					<div className="row">
-						<div className="left-col">
-							<div className="code-div">
-								{renderHTML(ReactivebaseMarkup)}
-							</div>
-						</div>
-						<div className="right-col">
-							<p><strong>ReactiveBase</strong> is the base wrapper component for all ReactiveSearch components that binds the back-end datastore with the UI components, allowing them to be reactively updated every time there is a change in the datastore or in the UI view components.</p>
-							<p>You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/getting-started/ReactiveBase.html" target="_blank">here</a>.</p>
-						</div>
-					</div>
+        <h5>Integrating Reactive Components</h5>
 
-					<div className="row">
-						<div className="left-col">
-							<div className="code-div">
-								{renderHTML(TextFieldMarkup)}
-							</div>
-						</div>
-						<div className="right-col">
-							<img src="https://i.imgur.com/DmDB9zF.png" />
-							<p><strong>TextField</strong> creates a simple text input field component that is optionally data connected. We’ll be using TextField here for adding new todo. You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/components/TextField.html" target="_blank">here</a>.</p>
-						</div>
-					</div>
+        <p>We will use <a href="https://opensource.appbase.io/reactivesearch/" target="_blank">ReactiveSearch</a> library for building reactive UI</p>
 
-					<div className="row">
-						<div className="left-col">
-							<div className="code-div">
-								{renderHTML(ToggleButtonMarkup)}
-							</div>
-						</div>
-						<div className="right-col">
-							<img src="https://i.imgur.com/91V7HPR.png" />
-							<p><strong>ToggleButton</strong> creates a toggle button UI component that is used for filtering results based on a fixed set of toggle-able options. We’ll be using ToggleButton here to filter “all”, “active”, “completed” todos. You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/components/ToggleButton.html" target="_blank">here</a>.</p>
-						</div>
-					</div>
+        <div className="docs-wrapper">
+          <div className="row">
+            <div className="left-col">
+              <div className="code-div">
+                {renderHTML(ReactivebaseMarkup)}
+              </div>
+            </div>
+            <div className="right-col">
+              <p><strong>ReactiveBase</strong> is the base wrapper component for all ReactiveSearch components that binds the back-end datastore with the UI components, allowing them to be reactively updated every time there is a change in the datastore or in the UI view components.</p>
+              <p>You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/getting-started/ReactiveBase.html" target="_blank">here</a>.</p>
+            </div>
+          </div>
 
-					<div className="row">
-						<div className="left-col">
-							<div className="code-div">
-								{renderHTML(ReactiveListMarkup)} <br/>
-							</div>
-						</div>
-						<div className="right-col">
-							<img src="https://i.imgur.com/ZUXaR28.png" />
-							<p><strong>ReactiveList</strong> is an actuator component to display results in a list layout, suited for data that needs a compact display. We’ll use ReactiveList to show the list of todos. You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/components/ReactiveList.html" target="_blank">here</a>.</p>
-						</div>
-					</div>
+          <div className="row">
+            <div className="left-col">
+              <div className="code-div">
+                {renderHTML(TextFieldMarkup)}
+              </div>
+            </div>
+            <div className="right-col">
+              <img src="https://i.imgur.com/DmDB9zF.png" />
+              <p><strong>TextField</strong> creates a simple text input field component that is optionally data connected. We’ll be using TextField here for adding new todo. You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/components/TextField.html" target="_blank">here</a>.</p>
+            </div>
+          </div>
 
-				</div>
-				<button className="btn btn-primary pos-static submit-btn" onClick={() => this.props.nextStep()}>
-					Next
-				</button>
-			</section>
-		);
-	}
+          <div className="row">
+            <div className="left-col">
+              <div className="code-div">
+                {renderHTML(ToggleButtonMarkup)}
+              </div>
+            </div>
+            <div className="right-col">
+              <img src="https://i.imgur.com/91V7HPR.png" />
+              <p><strong>ToggleButton</strong> creates a toggle button UI component that is used for filtering results based on a fixed set of toggle-able options. We’ll be using ToggleButton here to filter “all”, “active”, “completed” todos. You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/components/ToggleButton.html" target="_blank">here</a>.</p>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="left-col">
+              <div className="code-div">
+                {renderHTML(ReactiveListMarkup)} <br/>
+              </div>
+            </div>
+            <div className="right-col">
+              <img src="https://i.imgur.com/ZUXaR28.png" />
+              <p><strong>ReactiveList</strong> is an actuator component to display results in a list layout, suited for data that needs a compact display. We’ll use ReactiveList to show the list of todos. You can read more about it in the docs <a href="https://opensource.appbase.io/reactive-manual/v1.0.0/components/ReactiveList.html" target="_blank">here</a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <h5>Integrating Appbase-js</h5>
+        <p>We will use <a href="https://github.com/appbaseio/appbase-js" target="_blank">appbase-js</a> - a data streaming library for managing outgoing write requests</p>
+        <div className="docs-wrapper">
+          <div className="row">
+            <div className="left-col">
+              <div className="code-div">
+                {renderHTML(IndexMarkup)}
+              </div>
+            </div>
+            <div className="right-col">
+              <p><strong>index</strong> method writes a JSON data object at a given type and id location, or replaces if an object already exists.</p>
+              <p>This method is used while adding, toggling, editing a todo. If the id is existing in our dataset, then it will replace the body and if it is not existing, then it will create a new object.</p>
+              <p>You can read more about it in the docs <a href="http://docs.appbase.io/scalr/javascript/api-reference.html#javascript-api-reference-writing-data-index" target="_blank">here</a>.</p>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="left-col">
+              <div className="code-div">
+                {renderHTML(DeleteMarkup)}
+              </div>
+            </div>
+            <div className="right-col">
+              <p><strong>search</strong> method removes a JSON data object based on the id.</p>
+              <p>We will use this to remove/delete a todo and clearing completed todos.</p>
+              <p>You can read more about it in the docs <a href="http://docs.appbase.io/scalr/javascript/api-reference.html#javascript-api-reference-writing-data-delete" target="_blank">here</a>.</p>
+            </div>
+          </div>
+
+          <p>Next, we will need some methods to maintain the state of todos inside TodoModel</p>
+
+          <div className="row">
+            <div className="left-col">
+              <div className="code-div">
+                {renderHTML(SearchMarkup)}
+              </div>
+            </div>
+            <div className="right-col">
+              <p><strong>search</strong> method is used for matching documents in a type. It’s a convenience method for ElasticSearch’s /_search endpoint.</p>
+              <p>We will use it to initially load the todos in the TodoModel state.</p>
+              <p>You can read more about it in the docs <a href="http://docs.appbase.io/scalr/javascript/api-reference.html#javascript-api-reference-getting-data-search" target="_blank">here</a>.</p>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="left-col">
+              <div className="code-div">
+                {renderHTML(SearchStreamMarkup)}
+              </div>
+            </div>
+            <div className="right-col">
+              <p><strong>searchStream</strong> method is used for continuously streaming results of search query on a given type.</p>
+              <p>We will use it to stream the todos data in the TodoModel state.</p>
+              <p>You can read more about it in the docs <a href="http://docs.appbase.io/scalr/javascript/api-reference.html#javascript-api-reference-streaming-data-searchstream" target="_blank">here</a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <button className="btn btn-primary pos-static submit-btn" onClick={() => this.props.nextStep()}>
+          Next
+        </button>
+      </section>
+    );
+  }
 }

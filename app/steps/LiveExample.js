ec3a3a8bd6997b3278a34d29b2c0d99d13f33655
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2017-10-02 13:19:08 +0530

    Clean unused files, assets

--- app/steps/LiveExample.js
+++ app/steps/LiveExample.js
@@ -1,28 +1,12 @@
 import React, { Component } from "react";
 import { dataOperation } from "../service/DataOperation";
-
-import TodoApp from '../code/todoApp';
-import TodoModel from '../code/todoModel';
-// require("./styles.scss");
-// require("../code/reset.scss");
-
-let model = new TodoModel('react-todos')
+import LivePen from "./LivePen";
 
 export class LiveExample extends Component {
 
 	render() {
 		return (
-			<TodoApp model={model}/>
+			<LivePen>
 		);
 	}
 }
-
-// let render = () => {
-//   ReactDOM.render(
-//     <TodoApp model={model}/>,
-//     document.getElementsByClassName('todoapp')[0]
-//   )
-// }
-//
-// model.subscribe(render)
-// render()

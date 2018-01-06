4a8f6eaf0f6aaeedafca5aac5f5b1892fef3cf00
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-01-06 16:44:18 +0530

    Update SampleDataContainer

--- app/steps/SampleDataContainer.js
+++ app/steps/SampleDataContainer.js
@@ -1,45 +1,57 @@
 import React, { Component } from "react";
 import renderHTML from "react-render-html";
 
-const TodoMarkup = `<pre style='color:#000000;background:#ffffff;'><span style='color:#800080; '>{</span>
+const BookMarkup = `<pre style='color:#000000;background:#ffffff; border: 1px solid #ccc;'><span style='color:#800080; '>{</span>
     id<span style='color:#800080; '>:</span> <span style='color:#008c00; '>5696</span><span style='color:#808030; '>,</span>
-    isbn<span style='color:#800080; '>:</span> <span style='color:#800000; '>"</span><span style='color:#0000e6; '>718149866</span><span style='color:#800000; '>"</span><span style='color:#808030; '>,</span>
     title<span style='color:#800080; '>:</span> <span style='color:#800000; '>"</span><span style='color:#0000e6; '>The Brightest Star in the Sky</span><span style='color:#800000; '>"</span>
     authors<span style='color:#800080; '>:</span> <span style='color:#800000; '>"</span><span style='color:#0000e6; '>Marian Keyes</span><span style='color:#800000; '>"</span><span style='color:#808030; '>,</span>
     average_rating<span style='color:#800080; '>:</span> <span style='color:#008000; '>3.65</span><span style='color:#808030; '>,</span>
-    average_rating_rounded<span style='color:#800080; '>:</span> <span style='color:#008c00; '>4</span><span style='color:#808030; '>,</span>
-    books_count<span style='color:#800080; '>:</span> <span style='color:#008c00; '>49</span><span style='color:#808030; '>,</span>
-    language_code<span style='color:#800080; '>:</span> <span style='color:#800000; '>"</span><span style='color:#0000e6; '>en-GB</span><span style='color:#800000; '>"</span><span style='color:#808030; '>,</span>
     image<span style='color:#800080; '>:</span> <span style='color:#800000; '>"</span><span style='color:#0000e6; '>img_url</span><span style='color:#800000; '>"</span><span style='color:#808030; '>,</span>
-    image_medium<span style='color:#800080; '>:</span> <span style='color:#800000; '>"</span><span style='color:#0000e6; '>img_url_medium</span><span style='color:#800000; '>"</span><span style='color:#808030; '>,</span>
     original_publication_year<span style='color:#800080; '>:</span> <span style='color:#008c00; '>2009</span><span style='color:#808030; '>,</span>
-    original_title<span style='color:#800080; '>:</span> <span style='color:#800000; '>"</span><span style='color:#0000e6; '>The Brightest Star in the Sky</span><span style='color:#800000; '>"</span><span style='color:#808030; '>,</span>
-    original_series<span style='color:#800080; '>:</span> <span style='color:#800000; '>"</span><span style='color:#800000; '>"</span><span style='color:#808030; '>,</span>
-    ratings_count<span style='color:#800080; '>:</span> <span style='color:#008c00; '>17413</span><span style='color:#808030; '>,</span>
+    original_title<span style='color:#800080; '>:</span> <span style='color:#800000; '>"</span><span style='color:#0000e6; '>The Brightest Star in the Sky</span><span style='color:#800000; '>"</span>
 <span style='color:#800080; '>}</span>
 </pre>`;
 
-export default class SampleDataContainer extends Component {
+const DEJAVU_LINK =
+	"https://opensource.appbase.io/dejavu/live/#?input_state=XQAAAAKrAQAAAAAAAAA9iIqnY-B2BnTZGEQz6wkFsyzhBoa6J5YHVVPvvStg3duFL_9lBQxNAUEiS2LxrmQIi48IYsLycilGizdEqIf-Z3FUOIdIqHULMVrBqKtL5qUJx1gsOpt0WbuAhQS8qMoK8IdlqoG0tr-8UHi3sau8zMqY64fzpXCehrrPI4SNk8VTbiMsIZhduWAX4hCATwCBWfvrJqfAoiqKGt9zyTfsxLU7CbxGxE6__je7GeiC7UaPdD8YDeYC7eRxv-8JF1j3ysqY_Lkqc6hZAtUm9dN1Mg7O2uJ1MJxZyZWCmnz3ovLxz81T3C6KJZXI0OFjB5ll22UJm-zpscbqDsqx3Nz-reGuWWylUYoXRr0HBjalL-Feu_rGgiP_k7bcAA&editable=false";
 
+export default class SampleDataContainer extends Component {
 	render() {
 		return (
 			<section className="single-step">
-				<h2>Book Search storage structure</h2>
+				<h2>Book Search Dataset</h2>
 				<div className="docs-wrapper">
 					<div className="row">
 						<div className="left-col">
 							<div className="code-div">
-								{renderHTML(TodoMarkup)} <br/>
+								{renderHTML(BookMarkup)} <br />
 							</div>
 						</div>
-						<div className="right-col">
-							<p>Each book item would contain a unique <strong>id, isbn</strong> along with other book properties. <br/> <br/> We would be mostly focusing on <strong>original_title</strong>, <strong>authors</strong> image and <strong>average_rating_rounded</strong> to show in results.</p>
+						<div className="right-col step-one">
+							<p className="data-desc">
+								Each book item will contain a unique <strong>id</strong> along
+								with other book properties. <br /> <br /> In this app, we will
+								be using <strong>original_title</strong>,{" "}
+								<strong>authors, image, original_publication_year </strong> and{" "}
+								<strong>average_rating_rounded</strong> fields.
+								<br />
+								<br />
+								<span>
+									<span>Alternatively, you can access the</span>
+									<a href={DEJAVU_LINK} target="_blank" className="dataset-link">
+										full dataset over here.
+									</a>
+								</span>
+							</p>
 							{/* Hit the <strong>Next</strong> button at the bottom of the screen. */}
 						</div>
 					</div>
 				</div>
 
-				<button className="btn btn-primary pos-static submit-btn" onClick={() => this.props.nextStep()}>
+				<button
+					className="btn btn-primary pos-static submit-btn"
+					onClick={() => this.props.nextStep()}
+				>
 					Next
 				</button>
 			</section>
